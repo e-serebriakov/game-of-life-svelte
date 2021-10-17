bf4ea70009b3366fa9fc34e89a2801db9146e0c9
Евгений Серебряков <eserebryakov@Evgenijs-MacBook-Air.local>
2021-10-18 00:19:33 +0300

    Implement drawing by mouse moving

--- src/stores/settingsStore.js
+++ src/stores/settingsStore.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store'
 
 const STANDARD_LIFE_CHANCE = 0.15
 const STANDARD_CELL_SIZE = 20
-const DEFAULT_GRID_COLOR = '#38dbe0'
+const DEFAULT_GRID_COLOR = 'rgba(255, 255, 255, 0.2)'
 const DEFAULT_LIVE_CELL_COLOR = '#38dbe0'
 
 export const settingsStore = writable({
@@ -10,6 +10,7 @@ export const settingsStore = writable({
   paused: true,
   columns: 0,
   rows: 0,
+  gridShown: false,
   lifeChance: STANDARD_LIFE_CHANCE,
   cellSize: STANDARD_CELL_SIZE,
   gridColor: DEFAULT_GRID_COLOR,
